7c9988a3b572341439d275cf0f842fdc7b673c2f
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2021-01-27 21:33:39 +0100

    Ex. 9.8 done

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/ping', (_req, res) => {
+    res.send('pong');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
